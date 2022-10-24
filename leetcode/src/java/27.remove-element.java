9eed050135dfa63dfd9936c8b42d0266d5b02f20
wenjin1997 <wenjin1997@126.com>
2022-10-24 09:36:06 +0800

    27. Remove Element

---
+++ leetcode/src/java/27.remove-element.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=27 lang=java
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        // 双指针
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] != val) {
+                nums[left++] = nums[right];
+            }
+            right++;
+        }
+        return left;
+    }
+}
+// @lc code=end
+
